d3f4efe7ea32fa881a24c8b109cfaeec788e0014
Macos Barbosa <mbarbosa.ads@gmail.com>
2014-01-29 11:21:48 -0200

    Adicionando model Usuario

---
+++ app/Model/Usuario.php
@@ -0,0 +1,50 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Usuario Model
+ *
+ */
+class Usuario extends AppModel {
+
+/**
+ * Use database config
+ *
+ * @var string
+ */
+	public $useDbConfig = 'desenvolvimento';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'nome';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'nome' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'email' => array(
+			'email' => array(
+				'rule' => array('email'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
